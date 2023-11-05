727445cfdc0e6a8f162ebef4572b8f0b308203c3
abysssal <gamerlol5epic@gmail.com>
2023-11-05 08:26:13 -0500

    import db and sentient analysis libraries

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import discord
 import os
 import dotenv
+import textblob
+import tinydb
 
 dotenv.load_dotenv()
 token = str(os.getenv("TOKEN"))
 
-bot = discord.Bot()
+db = tinydb.TinyDB("./db.json")
+bot = discord.Bot(intents=discord.Intents().all())
 
 @bot.event
-async def on_ready():
-    print("dipshit")
+async def on_message():
+    blurb = textblob.TextBlob()
 
 bot.run(token)
